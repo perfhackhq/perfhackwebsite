f97835a15e5ce17ca8bec43ad7ed1f34d2c75661
perfhackdotcom <149968369+perfhackdotcom@users.noreply.github.com>
2023-11-07 02:07:17 +0530

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/sponsors', title: 'Sponsors' },
   { href: '/about', title: 'About' },
 ]
 

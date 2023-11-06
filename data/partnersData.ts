9034f63bc2dc710a37d02cd79312d663d9664371
aloke majumder <aloke.13@gmail.com>
2023-11-07 02:21:35 +0530

    Create partnersData.ts

---
+++ data/partnersData.ts
@@ -0,0 +1,16 @@
+const partnersData = [
+  {
+    title: 'hoichoi.tech',
+    description: `The Bengali OTT service`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.hoichoi.tv',
+  },
+  {
+    title: 'bong.dev',
+    description: `A Community of devs`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://bong.dev',
+  },
+]
+
+export default partnersData

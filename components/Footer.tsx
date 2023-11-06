c76d4baa6728aec31f00f31646a9b65b1af9c5e8
perfhackdotcom <149968369+perfhackdotcom@users.noreply.github.com>
2023-11-06 15:30:59 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,8 +22,9 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog" target="_blank">
+            Powered By Tailwind Nextjs Theme |  <Link href="https://vercel.com" target="_blank">
+            Hosted on Vercel
           </Link>
         </div>
       </div>
